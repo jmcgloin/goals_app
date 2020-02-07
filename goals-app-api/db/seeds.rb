7a3c2b5770a815dea63bf173f2bc87432950fec5
Jason McGloin <jason.mcgloin@gmail.com>
2020-02-06 22:23:58 -0500

    Adds seed data

--- goals-app-api/db/seeds.rb
+++ goals-app-api/db/seeds.rb
@@ -6,8 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-jason = User.create(username: 'Jason', email: 'j@son.com', password: 'password')
+require 'faker'
 
-proj = Goal.create(goalname: 'Project', deadline: '2020-01-24', importance: 10, user: jason)
-step1 = Step.create(stepname: 'backend', step_number: 1, completed: 0, goal: proj)
-step2 = Step.create(stepname: 'frontend', step_number: 2, completed: 0, goal: proj)
+testUser = User.create(username: 'Test', email: 't@est.com', password: 'asdfasfd')
+
+10.times {
+	Goal.create(
+		goalname: "Find #{Faker::JapaneseMedia::SwordArtOnline.item}",
+		deadline: '2020-01-24',
+		importance: 10,
+		user: testUser
+	)
+}
